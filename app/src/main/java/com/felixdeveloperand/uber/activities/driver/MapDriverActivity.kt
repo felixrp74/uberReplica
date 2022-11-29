5e660e6b169a82d2f05b9c0138525772984efa80
felixrp74 <felixrp74@gmail.com>
2022-11-28 21:28:50 -0500

    add googleMap inside driverActivity

--- app/src/main/java/com/felixdeveloperand/uber/activities/driver/MapDriverActivity.kt
+++ app/src/main/java/com/felixdeveloperand/uber/activities/driver/MapDriverActivity.kt
@@ -6,11 +6,17 @@ import android.os.Bundle
 import com.felixdeveloperand.uber.activities.MainActivity
 import com.felixdeveloperand.uber.databinding.ActivityMapDriverBinding
 import com.felixdeveloperand.uber.provider.AuthProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapDriverActivity : AppCompatActivity() {
+class MapDriverActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMapDriverBinding
     private lateinit var mAuthProvider: AuthProvider
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +34,14 @@ class MapDriverActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-0.19, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
