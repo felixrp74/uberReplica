a139bc7de5614d5c10decef1dfd62c170fa61ad1
felixrp74 <felixrp74@gmail.com>
2022-12-06 22:55:15 -0500

    add permission location in client activity

--- app/src/main/java/com/felixdeveloperand/uber/activities/client/MapClientActivity.kt
+++ app/src/main/java/com/felixdeveloperand/uber/activities/client/MapClientActivity.kt
@@ -1,31 +1,23 @@
 package com.felixdeveloperand.uber.activities.client
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
-import android.location.Location
-import android.location.LocationManager
-import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
+import android.os.Looper
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
-import com.felixdeveloperand.uber.activities.MainActivity
+import androidx.core.content.ContextCompat
 import com.felixdeveloperand.uber.databinding.ActivityMapClientBinding
 import com.felixdeveloperand.uber.provider.AuthProvider
 import com.felixdeveloperand.uber.util.showToast
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import java.util.*
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapClientActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -33,11 +25,10 @@ class MapClientActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mAuthProvider: AuthProvider
     private lateinit var mMap: GoogleMap
 
-    private val permissionId = 2
-
-    private lateinit var locationRequest: LocationRequest
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationCallback: LocationCallback
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mFusedLocation: FusedLocationProviderClient
+    private lateinit var mLocationCallback: LocationCallback
+    val LOCATION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,113 +37,102 @@ class MapClientActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mAuthProvider = AuthProvider()
 
-
-
-        binding.btnLocation.setOnClickListener {
-
-            getLocation()
-            /*mAuthProvider.logout()
-            Log.d("MapClient", "BECAUSE")
-
-            showToast("uber")
-
-            Intent(this@MapClientActivity, MainActivity::class.java).apply {
-                startActivity(this)
+        if (ContextCompat.checkSelfPermission(this@MapClientActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MapClientActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(this@MapClientActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+            }else{
+                ActivityCompat.requestPermissions(this@MapClientActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
             }
-            finish()
-
-             */
         }
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        /*mMap = googleMap
+        mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(-0.19, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-         */
+        mLocationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(1000)
+            .setFastestInterval(1000)
+            .setSmallestDisplacement(5.0f)
+
+        startLocation()
     }
 
-    @SuppressLint("MissingPermission", "SetTextI18n")
-    private fun getLocation() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
-                    val location: Location? = task.result
-                    if (location != null) {
-                        val geocoder = Geocoder(this, Locale.getDefault())
-                        val list: List<Address> =
-                            geocoder.getFromLocation(location.latitude, location.longitude, 1)
-                        binding.apply {
-                            tvLatitude.text = "Latitude\n${list[0].latitude}"
-                            tvLongitude.text = "Longitude\n${list[0].longitude}"
-                            tvCountryName.text = "Country Name\n${list[0].countryName}"
-                            tvLocality.text = "Locality\n${list[0].locality}"
-                            tvAddress.text = "Address\n${list[0].getAddressLine(0)}"
-                        }
-                    }else{
-                        showToast("MAS GRANDE DO MUNDO")
-                    }
-                }
-            } else {
-                showToast("Please turn on location")
-                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(intent)
-            }
-        } else {
-            requestPermissions()
-        }
-    }
-
-    private fun checkPermissions(): Boolean {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ),
-            permissionId
-        )
-    }
-
-    @SuppressLint("MissingSuperCall")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray
     ) {
-        if (requestCode == permissionId) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                getLocation()
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (LOCATION_REQUEST_CODE) {
+
+            1 -> {
+                if (ContextCompat.checkSelfPermission(this@MapClientActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    showToast("permission granted")
+//                    Looper.myLooper()?.let {
+//                        mFusedLocation.requestLocationUpdates(
+//                            mLocationRequest,
+//                            mLocationCallback,
+//                            it
+//                        )
+//                    }
+                }
             }
+
         }
     }
 
-    private fun isLocationEnabled(): Boolean {
-        val locationManager: LocationManager =
-            getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
+    private fun startLocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            Looper.myLooper()?.let {
+                mFusedLocation.requestLocationUpdates(
+                    mLocationRequest,
+                    mLocationCallback,
+                    it
+                )
+            }
+        }else{
+            checkLocationPermissions()
+        }
+    }
+
+    private fun checkLocationPermissions(){
+        if (
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                AlertDialog.Builder(this)
+                    .setTitle("Proporciona los permisos para continuar")
+                    .setMessage("Esta app requiere permisos de ubicacion")
+                    .setPositiveButton("OK") { dialog, id ->
+                        ActivityCompat.requestPermissions(
+                            this@MapClientActivity,
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE
+                        )
+
+                    }
+                    .create()
+                    .show()
+            }else{
+                ActivityCompat.requestPermissions(this@MapClientActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+            }
+        }
     }
 }
--- app/src/main/java/com/felixdeveloperand/uber/activities/driver/MapDriverActivity.kt
+++ app/src/main/java/com/felixdeveloperand/uber/activities/driver/MapDriverActivity.kt
@@ -1,15 +1,15 @@
 package com.felixdeveloperand.uber.activities.driver
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.felixdeveloperand.uber.activities.MainActivity
 import com.felixdeveloperand.uber.databinding.ActivityMapDriverBinding
 import com.felixdeveloperand.uber.provider.AuthProvider
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -31,9 +32,9 @@ class MapDriverActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
 
     private lateinit var mLocationRequest: LocationRequest
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var mFusedLocation: FusedLocationProviderClient
     private lateinit var mLocationCallback: LocationCallback
-    private val permissionId = 1
+    val LOCATION_REQUEST_CODE = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,17 +43,6 @@ class MapDriverActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         mAuthProvider = AuthProvider()
-
-
-        mLocationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult ?: return
-                for (location in locationResult.locations){
-                    // Update UI with location data
-                    // ...
-                }
-            }
-        }
 
         binding.btnLogoutDriver.setOnClickListener {
 
@@ -64,79 +54,101 @@ class MapDriverActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
 
+        mLocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
 
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MapDriverActivity)
+                for (location in locationResult.locations){
+                    // Update UI with location data
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                        CameraPosition.builder()
+                            .target(LatLng(location.latitude,location.longitude))
+                            .zoom(15.0f)
+                            .build()
+                    ))
+                }
+            }
+        }
 
-
+        mFusedLocation = LocationServices.getFusedLocationProviderClient(this@MapDriverActivity)
 
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-0.19, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        mMap.uiSettings.isZoomControlsEnabled = true
 
-        mLocationRequest = LocationRequest()
-        mLocationRequest.setInterval(300)
-        mLocationRequest.setFastestInterval(300)
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        mLocationRequest.setSmallestDisplacement(5.0f)
+        mLocationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(300)
+            .setFastestInterval(300)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setSmallestDisplacement(5.0f)
 
-
+        startLocation()
     }
 
-    @SuppressLint("MissingSuperCall")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray
     ) {
-        if (requestCode == permissionId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_REQUEST_CODE) {
             if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) == PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) == PackageManager.PERMISSION_GRANTED
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED
                 ) {
-                    mFusedLocationClient.requestLocationUpdates(
-                        mLocationRequest,
-                        mLocationCallback,
-                        Looper.myLooper()
-                    )
+                    Looper.myLooper()?.let {
+                        mFusedLocation.requestLocationUpdates(
+                            mLocationRequest,
+                            mLocationCallback,
+                            it
+                        )
+                    }
                 }
             }
         }
     }
 
-    fun checkLocationPermissions(){
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+    private fun startLocation(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            Looper.myLooper()?.let {
+                mFusedLocation.requestLocationUpdates(
+                    mLocationRequest,
+                    mLocationCallback,
+                    it
+                )
+            }
+        }else{
+            checkLocationPermissions()
+        }
+    }
+
+    private fun checkLocationPermissions(){
+        if (
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
         ){
             if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
                 AlertDialog.Builder(this)
                     .setTitle("Proporciona los permisos para continuar")
                     .setMessage("Esta app requiere permisos de ubicacion")
-                    .setPositiveButton("OK",
-                        DialogInterface.OnClickListener { dialog, id ->
+                    .setPositiveButton("OK") { dialog, id ->
+                        ActivityCompat.requestPermissions(
+                            this@MapDriverActivity,
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE
+                        )
 
-                        })
-                    .setNegativeButton("CANCEL",
-                        DialogInterface.OnClickListener { dialog, id ->
-                            // User cancelled the dialog
-                        })
+                    }
+                    .create()
+                    .show()
+            }else{
+                ActivityCompat.requestPermissions(this@MapDriverActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
             }
         }
     }
